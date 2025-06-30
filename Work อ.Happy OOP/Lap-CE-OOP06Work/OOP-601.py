554b4ba9f86f641a0a544c319a3ead63d06e2637
Kanaphat Rungrapeepornpong <changchana2123@gmail.com>
2025-06-30 18:04:30 +0700

    Delete Work อ.Happy OOP directory

--- Work อ.Happy OOP/Lap-CE-OOP06Work/OOP-601.py
+++
@@ -1,5 +0,0 @@
-print("_ _ _ _ This is a retangular area program _ _ _ _")
-L = float(input("Please, enter lenght of rectangular : "))
-W = float(input("Please, enter width of rectangular : "))
-Area = L*W
-print(f"Rectangular Area = {Area}")
